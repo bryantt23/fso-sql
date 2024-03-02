029bccebfe3431f4505811b884c51124795e65c1
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-03-02 00:40:21 -0800

    add middleware

--- models/index.js
+++ models/index.js
@@ -2,8 +2,10 @@ const Note = require('./note')
 const User = require('./user')
 const Blog = require('./blog')
 
-User.sync()
-Note.sync()
+User.hasMany(Note)
+Note.belongsTo(User)
+Note.sync({ alter: true })
+User.sync({ alter: true })
 Blog.sync()
 
 module.exports = {
